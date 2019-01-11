113c811c189c85a7d10647078b9ed6960d2be6f1
limingche <limingche@ylzinfo.com>
2019-01-11 11:24:47 +0800

    增加模块second；

---
+++ second/src/main/java/com/example/android/second/SecondMainActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.second;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity_main);
+    }
+}
